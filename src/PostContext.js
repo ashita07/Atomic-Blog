57e0365558ef31bf9808bb4ba280362aaf804580
Ashita <ashitam2407@gmail.com>
2025-02-10 18:14:41 +0530

    completed the atomic blog

--- src/PostContext.js
+++ src/PostContext.js
@@ -49,6 +49,10 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+
+  if (context === undefined) {
+    throw new Error("post context was used outside of the PostProvider");
+  }
   return context;
 }
 export { PostProvider, usePosts };
