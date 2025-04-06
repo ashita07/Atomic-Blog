d9f4f5998c52ce6adae50e1f7c13e697d9e6648c
Ashita <ashitam2407@gmail.com>
2025-04-06 21:28:41 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -136,31 +136,33 @@ function Archive() {
   const { onAddPost } = usePosts();
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
+    Array.from({ length: 100 }, () => createRandomPost())
   );
 
   const [showArchive, setShowArchive] = useState(false);
 
   return (
-    <aside>
-      <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
-      </button>
+    <>
+      <aside>
+        <h2>Post archive</h2>
+        <button onClick={() => setShowArchive((s) => !s)}>
+          {showArchive ? "Hide archive posts" : "Show archive posts"}
+        </button>
 
-      {showArchive && (
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>
-              <p>
-                <strong>{post.title}:</strong> {post.body}
-              </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </aside>
+        {showArchive && (
+          <ul>
+            {posts.map((post, i) => (
+              <li key={i}>
+                <p>
+                  <strong>{post.title}:</strong> {post.body}
+                </p>
+                <button onClick={() => onAddPost(post)}>Add as new post</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </aside>
+    </>
   );
 }
 
--- src/Test.js
+++ src/Test.js
@@ -14,13 +14,21 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function CompSlow({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
+  );
+}
+
+export default function Test() {
+  return (
+    <CompSlow>
+      <SlowComponent />
+    </CompSlow>
   );
 }
